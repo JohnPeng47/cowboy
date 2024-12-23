87a3d5ae6c75a5dc7409808ff8b03908c22ed767
Ubuntu <ubuntu@ip-172-31-37-242.us-east-2.compute.internal>
2024-12-23 17:14:23 -0500

    cleaned up some useless files

--- test_eval_dataset.py
+++
@@ -1,6 +0,0 @@
-from eval.eval_dataset import eval_dataset
-import asyncio
-
-asyncio.run(
-    eval_dataset("codecovapi-neutered", "codecovapi-neutered", num_records=1)
-)
--- test_read_data.py
+++
@@ -1,6 +0,0 @@
-from src.eval.datasets import read_rows, TestModuleEvalData
-
-dataset = read_rows("codecovapi-neutered")
-for d in dataset:
-    print(d.to_json().keys())
-    # print(TestModuleRow.from_json(d.to_json()))
